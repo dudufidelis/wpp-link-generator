1e739650ff31e2fa94c6f9c11a783545e602a580
Eduardo <duduecal12@gmail.com>
2024-09-24 17:21:56 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-document.querySelector("#generateBtn").addEventListener("click", () => {
+let btn = document.querySelector("#generateBtn");
+let refresh = document.querySelector("#refresh");
+
+btn.addEventListener("click", () => {
+    //get and transform into wpp link
     let inputNumber = document.querySelector("#phoneNumber").value;
     let phoneNumber = inputNumber.replace(/\D/g, "");
-    let wppLink = "https://wa.me/"
-    let res = wppLink.concat(phoneNumber)
-
-    document.querySelector("#result").innerHTML = res
-    document.querySelector("#whatsappBtn").href = res
+    let wppLink = "https://wa.me/";
+    let res = wppLink.concat(phoneNumber);
+    //change button to "a" tag with wpp link
+    let a = document.querySelector("#link");
+    a.href = res
+    a.classList.remove("none")    
+    btn.classList.add("none")
 })
+
+refresh.addEventListener("click", () => {
+    location.reload();
+});
